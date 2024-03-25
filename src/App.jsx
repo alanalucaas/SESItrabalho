38c0a2e532f11a5d2eac8dcadc75c413836a49b5
alanalucaas <alana.clara@hotmail.com>
2024-03-25 11:40:11 -0300

    subindo pro rep

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Card from "'./components/Card'"
+import './App.css'
+
+
+function App() {
+
+
+  return (
+  <>
+<Card/>
+<Card/>
+<Card/>
+<Card/>
+
+  
+  </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import Foto01 from "../assets/imgs/foto1.jpg"
+
+
+export default function Card(){
+    return(
+        <div>
+            <h2>paisagem 01</h2>
+            <img src="{Foto01}" alt="Paisagem" width={200}/>
+            <p>qualquer coisa</p>
+            <p>batata doce</p>
+
+        </div>
+    )
+}
